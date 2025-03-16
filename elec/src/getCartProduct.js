1053a4e03a8c5300244860270144d5ba10b2f4b8
khushi <kumarsinghajay516@gmail.com>
2025-03-16 04:04:53 +0000

    HomeAndAddToCart

---
+++ elec/src/getCartProduct.js
@@ -0,0 +1,14 @@
+import { updateCartValue } from "./src/updateCartValue";
+
+export const getCartProductFromLS = () => {
+  let cartProducts = localStorage.getItem("cartProductLS");
+  if (!cartProducts) {
+    return [];
+  }
+  cartProducts = JSON.parse(cartProducts);
+
+  //update the cart button value
+  updateCartValue(cartProducts);
+
+  return cartProducts;
+};
---
+++ elec/src/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+import products from './api/api.json'
+import { showProductContainer } from './homeProductCart.js';
+
+
+
+
+showProductContainer(products);
